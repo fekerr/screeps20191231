45b78a22c6b38e8ac7c17680b6a29679f9f8aa6e
Fred Kerr <fekerr@gmail.com>
2019-12-31 18:10:15 -0800

    revert

--- org.transcrypt.__runtime__.js
+++ org.transcrypt.__runtime__.js
@@ -1,4 +1,4 @@
-// Transcrypt'ed from Python, 2019-12-31 17:49:48
+// Transcrypt'ed from Python, 2019-12-31 18:07:55
 var __name__ = 'org.transcrypt.__runtime__';
 export var __envir__ = {};
 __envir__.interpreter_name = 'python';
